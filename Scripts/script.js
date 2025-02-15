ccf5863513636460594969b373570cea6f0771dc
Mattéo <mstampanoni@gaming.tech>
2025-02-15 17:59:42 +0100

    SetUp

---
+++ Scripts/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const plant = document.querySelector(".plant");
+    const shadow = document.querySelector(".shadow");
+    const shimmer = document.querySelector(".shimmer-text");
+    const programmingIcon = document.querySelector(".programming-icon");
+
+    plant.style.animationPlayState = "running";
+    shadow.style.animationPlayState = "running";
+    shimmer.style.animationPlayState = "running";
+    programmingIcon.style.animationPlayState = "running";
+});
+
